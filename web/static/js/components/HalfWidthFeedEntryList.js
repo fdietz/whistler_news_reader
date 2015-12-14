bcc762b3f2dd32570654c93d5a0ad53eeeb3ad12
Frederik Dietz <fdietz@gmail.com>
2015-12-14 19:56:25 +0100

    fix selection handling

--- web/static/js/components/HalfWidthFeedEntryList.js
+++ web/static/js/components/HalfWidthFeedEntryList.js
@@ -4,8 +4,12 @@ import HalfWidthFeedEntry from "./HalfWidthFeedEntry";
 export default class HalfWidthFeedEntryList extends Component {
 
   static propTypes = {
-    entries: PropTypes.array.isRequired,
-    currentEntry: PropTypes.object,
+    entries: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.string.isRequired
+    })).isRequired,
+    currentEntry: PropTypes.shape({
+      id: PropTypes.string.isRequired
+    }),
     // todos: PropTypes.arrayOf(PropTypes.shape({
     //   text: PropTypes.string.isRequired,
     //   completed: PropTypes.bool.isRequired
@@ -19,7 +23,7 @@ export default class HalfWidthFeedEntryList extends Component {
     return (
       <div className="half-width-item-list">
         {this.props.entries.map(function(entry, i) {
-          let isSelected = (entry && currentEntry && entry.guid === currentEntry.guid) || false;
+          let isSelected = (entry && currentEntry && entry.id === currentEntry.id) || false;
           return (
             <HalfWidthFeedEntry
               entry={entry}
