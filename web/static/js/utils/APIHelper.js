e6710f896a824f203d9daf3b03fd2acaf8f33285
Frederik Dietz <fdietz@gmail.com>
2016-04-01 11:22:18 +0200

    global axios response interceptor

--- web/static/js/utils/APIHelper.js
+++ web/static/js/utils/APIHelper.js
@@ -6,4 +6,13 @@ const instance = axios.create({
   headers: { Authorization: AuthToken.getToken() }
 });
 
+instance.interceptors.response.use(function (response) {
+  return response;
+}, function (error) {
+  if (error instanceof Error) {
+    console.log("Error", error.message);
+  }
+  return Promise.reject(error);
+});
+
 export default instance;
