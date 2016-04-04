df0e62c7818dbf6f8b8ce4b60a9e32550f7fcfa0
Frederik Dietz <fdietz@gmail.com>
2016-04-04 13:56:09 +0200

    cleanup feed importer test

--- test/models/feed_importer_test.exs
+++ test/models/feed_importer_test.exs
@@ -4,6 +4,9 @@ defmodule WhistlerNewsReader.FeedImporterTest do
   alias WhistlerNewsReader.Repo
   alias WhistlerNewsReader.Subscription
   alias WhistlerNewsReader.FeedImporter
+  alias WhistlerNewsReader.FeedFetcher
+
+  import Mock
 
   @feed_url "http://www.theverge.com/rss/frontpage"
   @not_existing_feed_url "http://www.theverge.com/404"
@@ -23,23 +26,25 @@ defmodule WhistlerNewsReader.FeedImporterTest do
   end
 
   test "fetch_and_parse succeeds" do
-    {:ok, feed_attrs } = FeedImporter.fetch_and_parse(@feed_url)
-    assert "http://www.theverge.com/rss/full.xml" == feed_attrs[:id]
+    json_body = File.read!("test/fixtures/rss2/example1.xml")
+    with_mock FeedFetcher, [fetch: fn(_feed_url) -> {:ok, json_body} end] do
+      {:ok, feed_attrs } = FeedImporter.fetch_and_parse(@feed_url)
+      assert "http://www.example.com" == feed_attrs[:url]
+    end
   end
 
   test "fetch_and_parse fails on HTTP not found error" do
-    {:error, :not_found } = FeedImporter.fetch_and_parse(@not_existing_feed_url)
-  end
-
-  test "fetch_and_parse fails on parsing invalid feed" do
-    {:error, error } = FeedImporter.fetch_and_parse(@invalid_feed_format_feed_url)
-    assert :not_found == error
+    with_mock FeedFetcher, [fetch: fn(_feed_url) -> {:error, :not_found} end] do
+      assert FeedImporter.fetch_and_parse(@not_existing_feed_url) == {:error, :not_found }
+    end
   end
 
   test "import_feed succeeds", %{user: user} do
-    {:ok, feed } = FeedImporter.import_feed(user, %{"feed_url" => @feed_url})
-    assert Repo.get_by!(Subscription, feed_id: feed.id)
-    assert "http://www.theverge.com/" == feed.site_url
-    assert "http://www.theverge.com/rss/frontpage" == feed.feed_url
+    json_body = File.read!("test/fixtures/rss2/example1.xml")
+    with_mock FeedFetcher, [fetch: fn(_feed_url) -> {:ok, json_body} end] do
+      {:ok, feed } = FeedImporter.import_feed(user, %{"feed_url" => @feed_url})
+      assert Repo.get_by!(Subscription, feed_id: feed.id)
+      assert "http://www.example.com" == feed.site_url
+    end
   end
 end
