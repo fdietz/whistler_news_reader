2b80b70c364081b1421352f12ba985c308332877
Frederik Dietz <fdietz@gmail.com>
2016-04-04 17:47:00 +0200

    add js tests for ButtonGroup, FeedEntry, FeedEntryList components

--- web/static/js/components/__tests__/FeedEntryList_test.js
+++ web/static/js/components/__tests__/FeedEntryList_test.js
@@ -4,8 +4,22 @@ import { shallow } from "enzyme";
 import FeedEntryList from "../FeedEntryList";
 import FeedEntry from "../FeedEntry";
 
-// test("FeedEntryList render count", t => {
-//   const props = { id: 1, title: "title", published: "2016-01-01", unread: true, summary: "summary", isSelected: true, onClick: () => {}, feed: { id: 1 }};
-//   const wrapper = shallow(<FeedEntryList entries={[props]} currentEntry={props} onEntryClick={() => {}}></FeedEntryList>);
-//   t.true(wrapper.contains("Test"));
-// });
+const props = {
+  id: 1,
+  title: "title",
+  published: "2016-01-01",
+  unread: true,
+  summary: "summary",
+  isSelected: false,
+  onClick: () => {},
+  feed: {
+    id: 1,
+    title: "feed title"
+  }
+};
+
+test("FeedEntryList renders correctly", t => {
+  const wrapper = shallow(<FeedEntryList entries={[props]} currentEntry={props} onEntryClick={() => {}}/>);
+  const feedEntry = wrapper.find(FeedEntry);
+  t.same(feedEntry.prop("title"), "title");
+});
--- web/static/js/components/__tests__/FeedEntry_test.js
+++ web/static/js/components/__tests__/FeedEntry_test.js
@@ -1,10 +1,38 @@
 import test from "ava";
 import React from "react";
 import { shallow } from "enzyme";
+import sinon from "sinon";
+
 import FeedEntry from "../FeedEntry";
 
-// test("FeedEntry render count", t => {
-//   const props = { id: 1, title: "title", published: "2016-01-01", unread: true, summary: "summary", isSelected: true, onClick: () => {}, feed: { id: 1 }};
-//   const wrapper = shallow(<FeedEntry {...props}/>);
-//   t.true(wrapper.contains("Test"));
-// });
+const props = {
+  id: 1,
+  title: "title",
+  published: "2016-01-01",
+  unread: true,
+  summary: "summary",
+  isSelected: false,
+  onClick: () => {},
+  feed: {
+    id: 1,
+    title: "feed title"
+  }
+};
+
+test("FeedEntry render correctly", t => {
+  const wrapper = shallow(<FeedEntry {...props}/>);
+  t.true(wrapper.find(".entry-title").text() === "title");
+  t.true(wrapper.find(".feed-title").text() === "feed title");
+});
+
+test("FeedEntry render circle if selected", t => {
+  const wrapper = shallow(<FeedEntry {...props} isSelected={true}/>);
+  t.true(wrapper.find(".circle").length === 1);
+});
+
+test("FeedEntry fire onClick", t => {
+  const spy = sinon.spy();
+  const wrapper = shallow(<FeedEntry {...props} onClick={spy}/>);
+  wrapper.simulate("click");
+  t.true(spy.calledOnce);
+});
