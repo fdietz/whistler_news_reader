aca4eb0b3f8042ab5ebb89aa092fe4cafe78645b
Frederik Dietz <fdietz@gmail.com>
2016-04-04 12:54:39 +0200

    cleanup feed fetcher

--- web/models/feed_fetcher.ex
+++ web/models/feed_fetcher.ex
@@ -1,14 +1,15 @@
 defmodule WhistlerNewsReader.FeedFetcher do
 
+  require Logger
+
   def fetch(feed_url) do
-    HTTPoison.start
     case HTTPoison.get(feed_url) do
     {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
       {:ok, body}
     {:ok, %HTTPoison.Response{status_code: 404}} ->
       {:error, :not_found}
     {:error, %HTTPoison.Error{reason: reason}} ->
-      IO.inspect reason
+      Logger.error(IO.inspect(reason))
       {:error, reason}
     end
   end
