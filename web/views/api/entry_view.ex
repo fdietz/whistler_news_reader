64c332d577712741f60fb59c657e16666c700f10
Frederik Dietz <fdietz@gmail.com>
2016-04-04 12:35:51 +0200

    cleanups views

--- web/views/api/entry_view.ex
+++ web/views/api/entry_view.ex
@@ -1,31 +1,15 @@
 defmodule WhistlerNewsReader.Api.EntryView do
   use WhistlerNewsReader.Web, :view
-  alias WhistlerNewsReader.UnreadEntry
-  alias WhistlerNewsReader.Repo
 
-  # TODO: refactor current_user handling
-  def render("index.json", %{entries: entries, current_user: current_user}) do
-    %{
-      entries: Enum.map(entries, fn(entry) ->
-        %{
-          id: entry.id,
-          title: entry.title,
-          published: entry.published,
-          author: entry.author,
-          summary: entry.summary,
-          content: entry.content,
-          url: entry.url,
-          unread: true,
-          feed: %{
-            id: entry.feed.id,
-            title: entry.feed.title
-          }
-        }
-      end)
-    }
+  def render("index.json", %{entries: entries}) do
+    %{entries: render_many(entries, WhistlerNewsReader.Api.EntryView, "entry.json")}
   end
 
-  def render("entry.json", %{entry: entry, current_user: current_user}) do
+  def render("show.json", %{entry: entry}) do
+    %{entry: render_one(entry, WhistlerNewsReader.Api.EntryView, "entry.json")}
+  end
+
+  def render("entry.json", %{entry: entry}) do
     %{
       id: entry.id,
       title: entry.title,
