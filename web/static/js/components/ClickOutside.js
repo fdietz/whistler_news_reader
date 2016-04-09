319e8e8ba26b1b43a58e0329f8c237266c025097
Frederik Dietz <fdietz@gmail.com>
2016-04-09 13:48:28 +0200

    add clickoutside component

---
+++ web/static/js/components/ClickOutside.js
@@ -0,0 +1,35 @@
+import React, { Component, PropTypes } from "react";
+
+export default class ClickOutside extends Component {
+
+  static propTypes = {
+    children: PropTypes.node,
+    onClickOutside: PropTypes.func.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  render() {
+    const { children } = this.props;
+    return <div {...this.props} ref="container">{children}</div>;
+  }
+
+  componentDidMount() {
+    document.addEventListener("click", this.handleClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("click", this.handleClick);
+  }
+
+  handleClick(e) {
+    const { onClickOutside } = this.props;
+    const el = this.refs.container;
+
+    if (!el.contains(e.target)) onClickOutside(e);
+  }
+}
