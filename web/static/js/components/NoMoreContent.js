631645c9f1a73bbae32802cfe6d51c85a0ddd895
Frederik Dietz <fdietz@gmail.com>
2016-04-15 13:23:45 +0200

    fix no more content styling

--- web/static/js/components/NoMoreContent.js
+++ web/static/js/components/NoMoreContent.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const NoMoreContent = () => {
   return (
-    <div className="item no-more-content">
+    <div className="entry-list__item no-more-content">
       <p className="hint">No more contents. You are all done!</p>
       <div className="smile">:-)</div>
     </div>
