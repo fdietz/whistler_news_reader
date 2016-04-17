27ec0ffcf0a60cfe512e864612a41af6946da562
Frederik Dietz <fdietz@gmail.com>
2016-04-17 18:22:39 +0200

    feed url extractor supports rss2 and atom alternate link element

--- test/models/feed_url_extractor_test.exs
+++ test/models/feed_url_extractor_test.exs
@@ -3,9 +3,15 @@ defmodule WhistlerNewsReader.FeedUrlExtractorTest do
 
   alias WhistlerNewsReader.FeedUrlExtractor
 
-  test "extract feed url" do
+  test "extract rss feed url" do
     json_body = File.read!("test/fixtures/html/example1.html")
     {:ok, feed_url} = FeedUrlExtractor.extract(json_body)
     assert feed_url == "http://example.com/rss2.xml"
+  end
+
+  test "extract atom feed url" do
+    json_body = File.read!("test/fixtures/html/example2.html")
+    {:ok, feed_url} = FeedUrlExtractor.extract(json_body)
+    assert feed_url == "http://example.com/atom.xml"
   end
 end
--- web/models/feed_url_extractor.ex
+++ web/models/feed_url_extractor.ex
@@ -1,12 +1,16 @@
 defmodule WhistlerNewsReader.FeedUrlExtractor do
 
+  # extract the following
+  # <link rel="alternate" type="application/rss+xml" href="http://example.com"/>
+  # <link rel="alternate" type="application/atom+xml" href="http://example.com"/>
   def extract(html) do
-    case Floki.find(html, "link[type='application/rss+xml']") do
-      [] ->
-        {:error, :feed_url_not_found}
-      [feed_url] ->
-        {:ok, Enum.at(Floki.attribute(feed_url, "href"), 0)}
-    end
+    rss_match = Floki.find(html, "link[type='application/rss+xml']")
+    atom_match = Floki.find(html, "link[type='application/atom+xml']")
+
+    {:ok, href(rss_match) || href(atom_match) }
   end
 
+  defp href(element) do
+    Enum.at(Floki.attribute(element, "href"), 0)
+  end
 end
