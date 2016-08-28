18499c3eadf22d803246777970a56f2ff894e0bd
Frederik Dietz <fdietz@gmail.com>
2016-08-28 16:06:30 +0200

    refactor(setup): webpack production build and digest cleanup

--- lib/mix/tasks/digest.ex
+++ lib/mix/tasks/digest.ex
@@ -2,7 +2,7 @@ defmodule Mix.Tasks.WhistlerNewsReader.Digest do
   use Mix.Task
 
   def run(args) do
-    Mix.Shell.IO.cmd "./node_modules/webpack/bin/webpack.js"
+    Mix.Shell.IO.cmd "npm run webpack:build:production"
     :ok = Mix.Tasks.Phoenix.Digest.run(args)
   end
 end
