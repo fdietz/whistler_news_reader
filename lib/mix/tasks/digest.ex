fa2b753242a0d397b139ccf2d5e0b4c19ae04925
Frederik Dietz <fdietz@gmail.com>
2016-09-11 20:23:52 +0200

    fix(webpack): digest npm run build

--- lib/mix/tasks/digest.ex
+++ lib/mix/tasks/digest.ex
@@ -2,7 +2,7 @@ defmodule Mix.Tasks.WhistlerNewsReader.Digest do
   use Mix.Task
 
   def run(args) do
-    Mix.Shell.IO.cmd "npm run webpack:build:production"
+    Mix.Shell.IO.cmd "npm run build:production"
     :ok = Mix.Tasks.Phoenix.Digest.run(args)
   end
 end
