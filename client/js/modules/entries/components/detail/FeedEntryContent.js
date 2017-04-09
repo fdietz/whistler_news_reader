c6d58cd251d66c9cc40467f6f1f72d926bd104b2
Frederik Dietz <fdietz@gmail.com>
2017-04-09 14:34:05 +0100

    refactor: re-add missing import

--- client/js/modules/entries/components/detail/FeedEntryContent.js
+++ client/js/modules/entries/components/detail/FeedEntryContent.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 
 import { findScrollableAncestor } from '../../../../utils/dom';
 import FeedEntryHeader from './FeedEntryHeader';
