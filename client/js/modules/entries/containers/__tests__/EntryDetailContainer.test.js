89f5d3c0f333d32fc8736510528d1ffb0df9e684
Frederik Dietz <fdietz@gmail.com>
2017-04-11 18:21:51 +0100

    test: add commented test for later

---
+++ client/js/modules/entries/containers/__tests__/EntryDetailContainer.test.js
@@ -0,0 +1,24 @@
+import renderer from 'react-test-renderer';
+import React from 'react';
+// import { EntryDetailContainer } from '../EntryDetailContainer';
+
+// mock wrapped connected component
+// jest.mock('../EntryContentToolbarContainer', () => 'EntryContentToolbarContainer');
+
+describe('EntryDetailContainer', () => {
+  it('renders correctly', () => {
+//     const tree = renderer.create(
+//       <EntryDetailContainer
+//         entries={[]}
+//         entry={{}}
+//         params={{}}
+//         pathname="path"
+//         decrementUnreadCount={() => {}}
+//         requestMarkEntryAsRead={() => {}}
+//         push={() => {}}
+//       />
+//     ).toJSON();
+
+//     expect(tree).toMatchSnapshot();
+  });
+});
