a6b15c7eb3068c57d06696d8a076d94def12abe3
Frederik Dietz <fdietz@gmail.com>
2017-04-16 15:54:32 +0200

    chore: update to elixir 1.4.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WhistlerNewsReader.Mixfile do
   def project do
     [app: :whistler_news_reader,
      version: "0.0.1",
-     elixir: "~> 1.4",
+     elixir: "~> 1.4.1",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
